965096f1fa2163652cd1875dbe92671b5c731aa1
KillersDeath <1076571392@qq.com>
2017-08-03 16:25:26 +0800

    test tag

--- mysite/corpustag/admin.py
+++ mysite/corpustag/admin.py
@@ -11,12 +11,12 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_display = ('question_text', 'tag', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
     search_fields = ['question_text']
     fieldsets = [
         (None, {
-            'fields': ['question_text']
+            'fields': ['question_text', 'tag']
         }),
         ('Date information', {
             'fields': ['pub_date'],
--- mysite/corpustag/models.py
+++ mysite/corpustag/models.py
@@ -3,17 +3,22 @@ from django.db import models
 import datetime
 from django.utils import timezone
 
-
 # Create your models here.
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=2000)
     pub_date = models.DateTimeField('date published')
+    TAG_NAMES = (('教育成长', (('校园生活', '校园生活'), ('考研出国', '考研出国'), )),
+                 ('党建团建', (('团务工作', '团务工作'), ('党务工作', '党务工作'), )),
+                 ('unknown', 'unknown'))
+
+    tag = models.CharField(max_length=10, choices=TAG_NAMES, default='unknown')
 
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'

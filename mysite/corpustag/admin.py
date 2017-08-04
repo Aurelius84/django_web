4f3f39d9597fc24b656e009d30fe4eec72fc7f6c
KillersDeath <1076571392@qq.com>
2017-08-04 10:58:20 +0800

    tag ok

--- mysite/corpustag/admin.py
+++ mysite/corpustag/admin.py
@@ -1,29 +1,30 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import YouthForumData
 
 # Register your models here.
 
 
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
+# class ChoiceInline(admin.TabularInline):
+#     model = Choice
+#     extra = 3
 
 
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text', 'tag', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
+class YouthForumDataAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'first_class', 'second_class', 'was_tagged_manually')
+    list_display_links = ('question_text',)
+    list_editable = ('first_class', 'second_class')
+    list_filter = ['first_class', 'second_class']
     search_fields = ['question_text']
     fieldsets = [
         (None, {
-            'fields': ['question_text', 'tag']
+            'fields': ['question_text', 'first_class', 'second_class']
         }),
         ('Date information', {
             'fields': ['pub_date'],
             'classes': ['collapse']
         }),
     ]
-    inlines = [ChoiceInline]
 
 
-admin.site.register(Question, QuestionAdmin)
+admin.site.register(YouthForumData, YouthForumDataAdmin)
--- mysite/corpustag/models.py
+++ mysite/corpustag/models.py
@@ -1,36 +1,34 @@
 from django.db import models
 
-import datetime
-from django.utils import timezone
-
 # Create your models here.
 
 
-class Question(models.Model):
+class YouthForumData(models.Model):
     question_text = models.CharField(max_length=2000)
     pub_date = models.DateTimeField('date published')
-    TAG_NAMES = (('教育成长', (('校园生活', '校园生活'), ('考研出国', '考研出国'), )),
-                 ('党建团建', (('团务工作', '团务工作'), ('党务工作', '党务工作'), )),
-                 ('unknown', 'unknown'))
+    FIRST_TAGS = (('教育成长', '教育成长'), ('党建团建', '党建团建'))
+    SECOND_TAGS = ((FIRST_TAGS[0][0], (('校园生活', '校园生活'), ('考研出国', '考研出国'))), (FIRST_TAGS[1][0], (('团务工作', '团务工作'),
+                   ('党务工作', '党务工作'))), ('unknown', 'unknown'))
 
-    tag = models.CharField(max_length=10, choices=TAG_NAMES, default='unknown')
+    first_class = models.CharField(max_length=20, choices=FIRST_TAGS, default='unknown')
+    second_class = models.CharField(max_length=20, choices=SECOND_TAGS, default='unknown')
 
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    def was_tagged_manually(self):
 
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
+        return 'unknown' not in [self.first_class, self.second_class]
+
+    was_tagged_manually.admin_order_field = 'pub_date'
+    was_tagged_manually.boolean = True
+    was_tagged_manually.short_description = 'already tagged?'
 
     def __str__(self):
         return self.question_text
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.choice_text
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
+#
+#     def __str__(self):
+#         return self.choice_text
